1eee6a8a5ea1d06543eda681af06ff7d7fbfc498
afrans <aluno@LTI2-030190.local.inatel.br>
2015-05-30 17:20:21 -0300

    erro na execução

---
+++ assets/js/GuestService.js
@@ -0,0 +1,31 @@
+var GuestService = {
+    
+    list: [],
+    
+    add: function(guest){
+        GuestService.list.push(guest);
+        GuestService.saveToLocalStorage();
+    },
+    
+    remove: function(guest){
+        //TODO to implemented
+    },
+    
+    getList: function(){
+        
+        return GuestService.list;
+    },
+    
+    saveToLocalStorage: function () {
+        var listJson = JSON.stringify(GuestService.list);
+        window.localStorage.setItem('guestlist',listJson);
+    },
+    
+    retrieveFromLocalStorage: function (){
+        var listJson = window.localStorage.getItem('guestlist');
+        if(listJson){
+            GuestService.list = JSON.parse(listJson);
+        }
+    }
+    
+}
